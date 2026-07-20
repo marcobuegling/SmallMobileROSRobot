fc7801a1232a7f5511100e19fcab9a99571986f5
Marco Bügling <marco.buegling@mailbox.org>
2026-07-20 22:09:59 +0000

    Added separate package for robot descriptions

---
+++ src/robot_py_descriptions/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_py_descriptions'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='marcobuegling',
+    maintainer_email='marco.buegling@mailbox.org',
+    description='Robot descriptions for Raspberry Pi and Python Mobile Robot Project',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
